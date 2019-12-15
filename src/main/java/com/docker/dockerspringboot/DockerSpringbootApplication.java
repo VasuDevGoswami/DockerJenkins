ffd28b37363231a849412d101ef351404441a9d4
Vasu_Dev1 <vasud1g@gmail.com>
2019-12-15 03:22:45 -0800

    first master commit

---
+++ src/main/java/com/docker/dockerspringboot/DockerSpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.docker.dockerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerSpringbootApplication {
+
+    @RequestMapping("/")
+    public String fun(){
+        return "Dockerised application";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSpringbootApplication.class, args);
+    }
+
+}
